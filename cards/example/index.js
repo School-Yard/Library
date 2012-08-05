e735efcda6a03d08c67b8ce688859272d92ff9d5
Cody Stoltman <particlebanana@gmail.com>
2012-08-05 17:21:26 -0500

    finish example user listing

--- cards/example/index.js
+++ cards/example/index.js
@@ -10,7 +10,13 @@
  * the Card-Catalog Card prototype.
  */
 var card_catalog = require('cardcatalog'),
-    util = require('utile');
+    util = require('util'),
+    path = require('path'),
+    User = require('./models/user'),
+    dustfs = require('dustfs');
+
+// Set template directory
+dustfs.dirs(path.join(__dirname + '/templates'));
 
 var Example = module.exports = function Example(options) {
   this.name = "Example"; // Required
@@ -39,43 +45,149 @@ var Example = module.exports = function Example(options) {
 util.inherits(Example, card_catalog.card);
 
 Example.prototype.index = function root(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Index page');
+  var self = this,
+      user = new User({ connection: req.storage.mongodb });
+
+  user.all(function(err, users) {
+    var data = {
+      users: users,
+      category: JSON.parse(category).slug,
+      card: self.slug
+    };
+
+    dustfs.render('index.dust', data, function(err, html) {
+      if(err) console.log('Error: '+err);
+      else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(html);
+      }
+    });
+  });
 };
 
 Example.prototype.newRecord = function newRecord(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Create a new record');
+  var self = this,
+      data = {
+        category: JSON.parse(category).slug,
+        card: self.slug
+      };
+
+  dustfs.render('new.dust', data, function(err, html) {
+    if(err) console.log('Error: '+err);
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+    }
+  });
 };
 
 Example.prototype.create = function create(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Created record');
+  var self = this,
+      user = new User({ connection: req.storage.mongodb });
+
+  user.create(req.body.user, function(err, result) {
+    if(err) console.log('Error: ' + err);
+
+    var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+    res.writeHead(302, {'Location': path});
+    res.end();
+  });
 };
 
 Example.prototype.show = function show(req, res, category) {
-  var args = Array.prototype.slice.call(arguments, 3).pop(),
-      str = ' ';
+  var self = this,
+      args = Array.prototype.slice.call(arguments, 3).pop(),
+      id = args.id,
+      user = new User({ connection: req.storage.mongodb });
 
-  Object.keys(args).forEach(function(k) {
-    str += k + ': ' + args[k];
-  });
+  if(!id) {
+    var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+    res.writeHead(302, {'Location': path});
+    res.end();
+  } else {
+    user.get(id, function(err, result) {
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Get record with params:' + str);
+      var data = {
+        user: result,
+        category: JSON.parse(category).slug,
+        card: self.slug
+      };
+
+      dustfs.render('show.dust', data, function(err, html) {
+        if(err) console.log('Error: '+err);
+        else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(html);
+        }
+      });
+    });
+  }
 };
 
 Example.prototype.edit = function edit(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Example edit page');
+  var self = this,
+      args = Array.prototype.slice.call(arguments, 3).pop(),
+      id = args.id,
+      user = new User({ connection: req.storage.mongodb });
+
+  if(!id) {
+    var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+    res.writeHead(302, {'Location': path});
+    res.end();
+  } else {
+    user.get(id, function(err, result) {
+
+      var data = {
+        user: result,
+        category: JSON.parse(category).slug,
+        card: self.slug
+      };
+
+      dustfs.render('edit.dust', data, function(err, html) {
+        if(err) console.log('Error: '+err);
+        else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(html);
+        }
+      });
+    });
+  }
 };
 
 Example.prototype.update = function update(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Example update page');
+  var self = this,
+      args = Array.prototype.slice.call(arguments, 3).pop(),
+      id = args.id,
+      user = new User({ connection: req.storage.mongodb });
+
+  if(!id) {
+    var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+    res.writeHead(302, {'Location': path});
+    res.end();
+  } else {
+    user.update(id, req.body.user, function(err, result) {
+      var path = '/' + JSON.parse(category).slug + '/' + self.slug + '/' + id;
+      res.writeHead(302, {'Location': path});
+      res.end();
+    });
+  }
 };
 
 Example.prototype.destroy = function destroy(req, res, category) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Example destroy page');
+  var self = this,
+      args = Array.prototype.slice.call(arguments, 3).pop(),
+      id = args.id,
+      user = new User({ connection: req.storage.mongodb });
+
+  if(!id) {
+    var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+    res.writeHead(302, {'Location': path});
+    res.end();
+  } else {
+    user.destroy(id, function(err, result) {
+      var path = '/' + JSON.parse(category).slug + '/' + self.slug;
+      res.writeHead(302, {'Location': path});
+      res.end();
+    });
+  }
 };
