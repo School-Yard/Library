8f939481d2f7f339e1f3d57ad14a9dff8dcec3d8
Matt McFarland <matmcfarland@gmail.com>
2012-10-05 16:54:13 -0500

    fixed clean database functionality

--- cards/content/tests/support/helpers.js
+++ cards/content/tests/support/helpers.js
@@ -2,18 +2,20 @@ var TK = require('trapperkeeper'),
     Helpers = module.exports = exports;
 
 
-Helpers.MockConnection = function(cb) {
+Helpers.MockConnection = function(callback) {
   var self = this;
-  this._resource = TK.connect('mongodb', 'mongodb://127.0.0.1', 27017, {database: 'library_testing'});
+  this._resource = TK.connect('mongodb', 'mongodb://127.0.0.1', 27017, {database: 'test'});
   this._resource.on('ready', function() {
-    return cb({
-      adapter: {
-        mongo: self._resource
-      }
+    Helpers.Clean(function() {
+      return callback({
+        adapter: {
+          mongo: self._resource
+        }
+      });
     });
   });
 };
 
-Helpers.Clean = function() {
-  this._resource.connection.db.dropDatabase();
+Helpers.Clean = function(callback) {
+  this._resource.connection.db('test').dropDatabase(callback);
 };
